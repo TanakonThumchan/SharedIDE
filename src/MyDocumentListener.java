5a52c5b917d27055a2498705a439d162e1467271
TanakonThumchan <33892002+TanakonThumchan@users.noreply.github.com>
2018-02-09 22:03:41 +0100

    Giorno 2
    Introduzione del DocumentListener per rilevare i cambiamenti dei testi nel componente JTextArea

---
+++ src/MyDocumentListener.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JOptionPane;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class MyDocumentListener implements DocumentListener {
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        //Plain text components do not fire these events
+        JOptionPane.showMessageDialog(null, "cambio");
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        JOptionPane.showMessageDialog(null, "scrivi");
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        JOptionPane.showMessageDialog(null, "togli");
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
