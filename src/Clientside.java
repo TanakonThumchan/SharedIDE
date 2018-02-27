f9ccfecd439bb7bdcfa19a29c75d619de79fcb22
TanakonThumchan <33892002+TanakonThumchan@users.noreply.github.com>
2018-02-27 22:09:43 +0100

    Sessione 8
    Aggiunta della classe ListenGlobal con l'uso di socketmulticast in ascolto per un eventuale rischiesta di connessione

--- src/Clientside.java
+++ src/Clientside.java
@@ -3,8 +3,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 /*
@@ -19,9 +24,14 @@ import javax.swing.*;
 public class Clientside {
 
     Socket socket = null;
+    InetAddress group=null;
+    MulticastSocket s=null;
     Clientside(){
         try {
             socket = new Socket(InetAddress.getLocalHost(), 10000);
+            InetAddress group = InetAddress.getByName("228.5.6.7");
+            MulticastSocket s = new MulticastSocket(6789);
+            s.joinGroup(group);
         } catch (IOException e) {
             JOptionPane.showConfirmDialog(null, "Connessione Fallita", "Connessione", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
         }
@@ -36,4 +46,20 @@ public class Clientside {
         }
         return temp;
     }
+    public String writemulti()
+    {
+        String msg = "Hello";
+        DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(),group, 6789);
+        byte[] buf = new byte[1000];
+        DatagramPacket recv = new DatagramPacket(buf, buf.length);
+        try {
+            s.send(hi);            
+            s.receive(recv);
+        } catch (IOException ex) {
+            JOptionPane.showConfirmDialog(null, "NO", "Connessione", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+        }
+        return buf.toString();
+    }
 }
+    
+    
