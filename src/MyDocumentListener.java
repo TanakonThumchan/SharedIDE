1a4d1fc8974b044a0d98a1da7f1dd1f000f513c4
TanakonThumchan <33892002+TanakonThumchan@users.noreply.github.com>
2018-03-02 22:18:04 +0100

    Sessione 9
    Gestione dei conflitti tra i Thread

--- src/MyDocumentListener.java
+++ src/MyDocumentListener.java
@@ -6,18 +6,22 @@ import javax.swing.event.DocumentListener;
 public class MyDocumentListener implements DocumentListener {
     @Override
     public void changedUpdate(DocumentEvent e) {
+        JOptionPane.showMessageDialog(null, ""+Thread.currentThread().getName());
         //Plain text components do not fire these events
         //JOptionPane.showMessageDialog(null, "cambio");
     }
 
     @Override
     public void insertUpdate(DocumentEvent e) {
+        if (!Thread.currentThread().getName().equals("CIAO"))
+        JOptionPane.showMessageDialog(null, "add "+Thread.currentThread().getName());
         //JOptionPane.showMessageDialog(null, "scrivi");
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void removeUpdate(DocumentEvent e) {
+        JOptionPane.showMessageDialog(null, "remo "+Thread.currentThread().getName());
         //JOptionPane.showMessageDialog(null, "togli");
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
